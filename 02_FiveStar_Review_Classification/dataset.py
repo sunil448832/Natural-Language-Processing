40c5887674bb184ec2f8491d6d2a1f0ee40437fa
sunil448832 <sunil4832sharma@gmail.com>
2021-03-20 21:31:38 +0530

    Initial Commit

--- 02_FiveStar_Review_Classification/dataset.py
+++ 02_FiveStar_Review_Classification/dataset.py
@@ -2,10 +2,11 @@ import re
 import json
 import torch
 import numpy as np
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from fastpunct import FastPunct
 fastpunct = FastPunct()
+
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -13,138 +14,143 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
-def pad(seq,max_len):
-  paddded_seq=seq.copy()
-  seq_len=len(seq)
-  paddded_seq=paddded_seq+['<pad>']*(max_len-len(seq)) \
-               if seq_len<max_len else seq[:max_len]
-  return paddded_seq
+
+def pad(seq, max_len):
+    paddded_seq = seq.copy()
+    seq_len = len(seq)
+    paddded_seq = paddded_seq+['<pad>']*(max_len-len(seq)) \
+        if seq_len < max_len else seq[:max_len]
+    return paddded_seq
 
 
 class MyDataset(Dataset):
-  def __init__(self,data,labels):
-    super(MyDataset,self).__init__()
-    self.data=data
-    self.labels=labels
+    def __init__(self, data, labels):
+        super(MyDataset, self).__init__()
+        self.data = data
+        self.labels = labels
 
-  def __getitem__(self,index):
-    return self.data[index] ,self.labels[index]
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
 
-  def __len__(self):
-    return len(self.data)
+    def __len__(self):
+        return len(self.data)
+
 
 class TextLoader:
-  def __init__(self):
-    self.words=[]
-    self.vectors = []
-    self.word_index = {}
-    self.text=[]
-    self.embed_size=None
-  
-  def punc_remove(self,text,correct=False):
-    text_seg,text_seg_idx=[],[0]
-    for i in range(len(text)):
-      text[i]=re.sub(r'[^\w\s\?\.\,]', '', text[i])
-      seq=re.split(" and |[^a-zA-Z ]+",text[i])
-      seq_=[i for i in seq if len(i)>1]
-      if len(seq_)<1:seq_.append("unknown")
-      text_seg.extend(seq_)
-      text_seg_idx.append(text_seg_idx[-1]+len(seq_))
+    def __init__(self):
+        self.words = []
+        self.vectors = []
+        self.word_index = {}
+        self.text = []
+        self.embed_size = None
 
-    text_corr= fastpunct.punct(text_seg, correct=True) if correct else text_seg
+    def punc_remove(self, text, correct=False):
+        text_seg, text_seg_idx = [], [0]
+        for i in range(len(text)):
+            text[i] = re.sub(r'[^\w\s\?\.\,]', '', text[i])
+            seq = re.split(" and |[^a-zA-Z ]+", text[i])
+            seq_ = [i for i in seq if len(i) > 1]
+            if len(seq_) < 1:
+                seq_.append("unknown")
+            text_seg.extend(seq_)
+            text_seg_idx.append(text_seg_idx[-1]+len(seq_))
 
-    for i in range(len(text)):
-      seq=text_corr[text_seg_idx[i]:text_seg_idx[i+1]]
-      text[i]=" ".join(i for i in seq)
+        text_corr = fastpunct.punct(
+            text_seg, correct=True) if correct else text_seg
 
-    return text
+        for i in range(len(text)):
+            seq = text_corr[text_seg_idx[i]:text_seg_idx[i+1]]
+            text[i] = " ".join(i for i in seq)
 
-  def glove(self,glove_file):
-    with open(glove_file, 'rb') as glove_file:
-        for idx, l in enumerate(glove_file):
-            line = l.decode().split()
-            word,vector = line[0],np.array(line[1:]).astype(np.float)
-            self.words.append(word)
-            self.vectors.append(vector)
-            self.word_index[word] = idx+self.pad_idx
+        return text
 
-  def tokenize(self,batch,build=False,max_len=100):
-    batch_token,batch_index=[],[]
+    def glove(self, glove_file):
+        with open(glove_file, 'rb') as glove_file:
+            for idx, l in enumerate(glove_file):
+                line = l.decode().split()
+                word, vector = line[0], np.array(line[1:]).astype(np.float)
+                self.words.append(word)
+                self.vectors.append(vector)
+                self.word_index[word] = idx+self.pad_idx
 
-    for seq in batch:
-      sentence=seq.replace("."," <eos>")
-      tokens=[]
-      
-      for token in sentence.lower().split(" "):
-        try:
-          _=self.word_index[token]
-          tokens.append(token)
-        except KeyError:
-          if build:
-            tokens.append(token)
-            self.words.append(token)
-            self.word_index[token]=len(self.vectors)
-            self.vectors.append(np.random.normal(scale=0.6, size=(self.embed_size, )))
-          elif not build:
-            tokens.append("<unk>")
+    def tokenize(self, batch, build=False, max_len=100):
+        batch_token, batch_index = [], []
 
-      if not build:
-       
-        tokens=pad(tokens,max_len)
-        token_index=[self.word_index[i] for i in tokens]
-        batch_token.append(tokens)
-        batch_index.append(token_index)
+        for seq in batch:
+            sentence = seq.replace(".", " <eos>")
+            tokens = []
 
-    return batch_token,batch_index
+            for token in sentence.lower().split(" "):
+                try:
+                    _ = self.word_index[token]
+                    tokens.append(token)
+                except KeyError:
+                    if build:
+                        tokens.append(token)
+                        self.words.append(token)
+                        self.word_index[token] = len(self.vectors)
+                        self.vectors.append(np.random.normal(
+                            scale=0.6, size=(self.embed_size, )))
+                    elif not build:
+                        tokens.append("<unk>")
 
-  def build_vocab(self,text,embed_size,vocab_file=None,glove_file=None,write_file=None,correct=False):
-    self.embed_size=embed_size
-    if vocab_file is not None:
-      with open(vocab_file) as file:
-        vocab_file_json=json.load(file)
-        self.words=vocab_file_json["words"]
-        self.word_index=vocab_file_json["word_index"]
-        self.vectors=vocab_file_json["vectors"]
-        self.text=vocab_file_json["text"]
+            if not build:
 
+                tokens = pad(tokens, max_len)
+                token_index = [self.word_index[i] for i in tokens]
+                batch_token.append(tokens)
+                batch_index.append(token_index)
 
-    else:
-      self.word_index['<unk>']=len(self.vectors) 
-      self.vectors.append(np.random.normal(scale=0.6, size=(embed_size, )))  
-      self.word_index['<eos>']=len(self.vectors) 
-      self.vectors.append(np.random.normal(scale=0.6, size=(embed_size, ))) 
-      self.pad_idx=len(self.vectors)
-      self.word_index['<pad>']=self.pad_idx
-      self.vectors.append(np.zeros(embed_size))
+        return batch_token, batch_index
 
-      if glove_file is not None:
-        self.glove(glove_file)
-      ds=MyDataset(text,text)
-      for txt,_ in tqdm(DataLoader(ds,batch_size=32,shuffle=False)):
-        txt=list(txt) if isinstance(txt,tuple) else txt
-        text_corr=self.punc_remove(txt,correct)
-        self.text.extend(text_corr)
-        _,_=self.tokenize(text_corr,build=True)
+    def build_vocab(self, text, embed_size, vocab_file=None, glove_file=None, write_file=None, correct=False):
+        self.embed_size = embed_size
+        if vocab_file is not None:
+            with open(vocab_file) as file:
+                vocab_file_json = json.load(file)
+                self.words = vocab_file_json["words"]
+                self.word_index = vocab_file_json["word_index"]
+                self.vectors = vocab_file_json["vectors"]
+                self.text = vocab_file_json["text"]
 
-      if write_file is not None:
-        vocab_file_json={}
-        vocab_file_json["words"]=self.words
-        vocab_file_json["word_index"]=self.word_index
-        vocab_file_json["vectors"]=self.vectors
-        vocab_file_json["text"]=self.text
-  
-        with open(write_file,'w') as file:
-          json.dump(vocab_file_json,file,cls=NumpyEncoder)
+        else:
+            self.word_index['<unk>'] = len(self.vectors)
+            self.vectors.append(np.random.normal(
+                scale=0.6, size=(embed_size, )))
+            self.word_index['<eos>'] = len(self.vectors)
+            self.vectors.append(np.random.normal(
+                scale=0.6, size=(embed_size, )))
+            self.pad_idx = len(self.vectors)
+            self.word_index['<pad>'] = self.pad_idx
+            self.vectors.append(np.zeros(embed_size))
 
+            if glove_file is not None:
+                self.glove(glove_file)
+            ds = MyDataset(text, text)
+            for txt, _ in tqdm(DataLoader(ds, batch_size=32, shuffle=False)):
+                txt = list(txt) if isinstance(txt, tuple) else txt
+                text_corr = self.punc_remove(txt, correct)
+                self.text.extend(text_corr)
+                _, _ = self.tokenize(text_corr, build=True)
 
-  def postprocess(self,batch,max_len=200,correct=False ,lm=False):
-    batch=list(batch) if isinstance(batch,tuple) else batch
-    batch=self.punc_remove(batch,correct)
-    if lm:
-      _,data_=self.tokenize(batch,max_len=max_len)
-      data_=np.array(data_)
-      data,target=data_[:,:-1],data_[:,1:]
-      return torch.LongTensor(data),torch.LongTensor(target)
-    else:
-      _,data=self.tokenize(batch,max_len=max_len)
-      return torch.LongTensor(data)
+            if write_file is not None:
+                vocab_file_json = {}
+                vocab_file_json["words"] = self.words
+                vocab_file_json["word_index"] = self.word_index
+                vocab_file_json["vectors"] = self.vectors
+                vocab_file_json["text"] = self.text
+
+                with open(write_file, 'w') as file:
+                    json.dump(vocab_file_json, file, cls=NumpyEncoder)
+
+    def postprocess(self, batch, max_len=200, correct=False, lm=False):
+        batch = list(batch) if isinstance(batch, tuple) else batch
+        batch = self.punc_remove(batch, correct)
+        if lm:
+            _, data_ = self.tokenize(batch, max_len=max_len)
+            data_ = np.array(data_)
+            data, target = data_[:, :-1], data_[:, 1:]
+            return torch.LongTensor(data), torch.LongTensor(target)
+        else:
+            _, data = self.tokenize(batch, max_len=max_len)
+            return torch.LongTensor(data)
--- 02_FiveStar_Review_Classification/utils.py
+++ 02_FiveStar_Review_Classification/utils.py
@@ -2,20 +2,22 @@ import re
 import torch
 import numpy as np
 
+
 def remove_emoji(string):
     emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', string)
 
-    
-def accuracy(preds, y,lm=False):
-    _, indices =preds.max(dim=2) if lm  else preds.max(dim=1)
-    correct =(indices.reshape(-1) == y.reshape(-1)).float()
+
+def accuracy(preds, y, lm=False):
+    _, indices = preds.max(dim=2) if lm else preds.max(dim=1)
+    correct = (indices.reshape(-1) == y.reshape(-1)
+               ).float() if lm else (indices == y-1).float()
     acc = correct.sum()/len(correct)
     return acc
